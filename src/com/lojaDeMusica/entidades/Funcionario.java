82c2b58d9b2d1dad33496552fcb424c2e4481031
Heitor Costa Silva <heitorcostasilva5@gmail.com>
2025-10-16 23:21:00 -0300

    uu

---
+++ src/com/lojaDeMusica/entidades/Funcionario.java
@@ -0,0 +1,13 @@
+package com.lojaDeMusica.entidades;
+
+public class Funcionario {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        // prints the result of 2 + 2
+        System.out.println(add(2, 2));
+    }
+}
