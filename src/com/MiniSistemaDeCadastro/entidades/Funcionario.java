adfb44b796cc5218ae6588c82283d7235ff3dc18
Heitor Costa Silva <heitorcostasilva5@gmail.com>
2025-10-17 00:47:17 -0300

    adiciona a classe Funcionario com atributos e métodos necessários

--- src/com/MiniSistemaDeCadastro/entidades/Funcionario.java
+++ src/com/MiniSistemaDeCadastro/entidades/Funcionario.java
@@ -1,6 +1,47 @@
 package com.MiniSistemaDeCadastro.entidades;
 public class Funcionario {
+    private int id;
+    private String nome;
+    private double salario;
+    private String matricula;
 
-      
-    
+    public Funcionario(int id, String nome, double salario, String matricula) {
+        this.id = id;
+        this.nome = nome;
+        this.salario = salario;
+        this.matricula = matricula;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Nome: " + nome + " | Salário: " + salario + " | Matrícula: " + matricula;
+    }
 }
